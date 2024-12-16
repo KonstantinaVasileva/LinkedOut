253e7aeb1d2bf00bb262df1a1ce257628541e429
Konstantina Vasileva <konstantinavasileva@gmail.com>
2024-12-16 18:55:54 +0200

    Add company form validation and i18n message configuration

--- src/main/java/bg/softuni/LinkedOut/web/AddCompanyController.java
+++ src/main/java/bg/softuni/LinkedOut/web/AddCompanyController.java
@@ -4,6 +4,7 @@ import bg.softuni.LinkedOut.model.dto.AddCompanyDTO;
 import bg.softuni.LinkedOut.service.CompanyService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,12 +16,16 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class AddCompanyController {
 
     private final CompanyService companyService;
+
     public AddCompanyController(CompanyService companyService) {
         this.companyService = companyService;
     }
 
     @GetMapping("/add")
-    public String addCompany() {
+    public String addCompany(Model model) {
+        if (!model.containsAttribute("addCompanyDTO")) {
+            model.addAttribute("addCompanyDTO", new AddCompanyDTO(null, null, null, null));
+        }
         return "company-add";
     }
 
@@ -29,15 +34,16 @@ public class AddCompanyController {
                                     BindingResult bindingResult,
                                     RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("companyAddForm", addCompanyDTO);
+            redirectAttributes.addFlashAttribute("addCompanyDTO", addCompanyDTO);
             redirectAttributes.addFlashAttribute(
-                    "org.springframework.validation.BindingResult.companyAddForm", bindingResult);
+                    "org.springframework.validation.BindingResult.addCompanyDTO",
+                    bindingResult);
             return "redirect:/companies/add";
         }
 
         long id = companyService.addCompany(addCompanyDTO);
 
-        return "redirect:/companies/"+ id;
+        return "redirect:/companies/" + id;
     }
 
 }
