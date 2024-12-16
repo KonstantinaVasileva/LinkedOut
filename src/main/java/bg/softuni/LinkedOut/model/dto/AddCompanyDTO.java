92f8a13d596c91f0e1087a285b7560126d8af93a
Konstantina Vasileva <konstantinavasileva@gmail.com>
2024-12-16 13:48:48 +0200

    Add validation messages for AddCompanyDTO fields

--- src/main/java/bg/softuni/LinkedOut/model/dto/AddCompanyDTO.java
+++ src/main/java/bg/softuni/LinkedOut/model/dto/AddCompanyDTO.java
@@ -6,13 +6,16 @@ import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 
 public record AddCompanyDTO(
-        @Size(min = 2, max = 10)
+        @Size(min = 2, max = 10,
+        message = "{add.company.name.length}")
         String name,
-        @Size(min = 2, max = 10)
+        @Size(min = 2, max = 10,
+        message = "{add.company.town.length}")
         String town,
-        @Size(min = 10)
+        @Size(min = 10,
+        message = "{add.company.description.length}")
         String description,
-        @Positive
+        @Positive(message = "{add.company.budget.positive}")
         BigDecimal budget
 ) {
 }
